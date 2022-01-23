c31bc183e97c8be6026d25288184b0af43bf3c19
sh20raj <shaswatraj3@gmail.com>
2022-01-23 16:52:14 +0530

    Just Start - Simplehtmldom added

---
+++ get/website-data.php
@@ -0,0 +1,31 @@
+<?php
+include '../assets/simplehtmldom.php';
+// Create DOM from URL or file
+if(isset($_GET['url'])){$url = $_GET['url'];}else{$url = "https://www.youtube.com/channel/UCFPFrVOaeG3q1PEzpeh7hCg";}
+
+$html = file_get_html($url);
+
+
+echo $title = $html->find('title',0)->plaintext;
+print_r($title = $html->find('meta[property="og:image"]',0)) ;
+print_r('ff');
+
+
+foreach($html->find('meta') as $element)
+       echo $element->src . '<br>';
+
+
+
+
+
+/*
+// Find all images
+foreach($html->find('img') as $element)
+       echo $element->src . '<br>';
+
+// Find all links
+foreach($html->find('a') as $element)
+       echo $element->href . '<br>';
+*/
+
+?>
