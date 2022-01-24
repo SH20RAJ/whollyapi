4bbabbc0934d47d9926f6a0c306fd877b0ce0ab1
Shaswat Raj <66713844+SH20RAJ@users.noreply.github.com>
2022-01-24 21:52:08 +0530

    Update details.php

--- websites/pngaaa.com/details.php
+++ websites/pngaaa.com/details.php
@@ -2,6 +2,7 @@
 include 'simple_html_dom.php';
 // Create DOM from URL or file
 header('Content-Type: application/json; charset=utf-8');
+header('Access-Control-Allow-Origin: *');
 
 if(isset($_GET['id'])){
     $q=$_GET['id'];
