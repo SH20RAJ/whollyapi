60b10062d0786d5e0590571cbc2b50ba5763ae71
Shaswat Raj <shaswatraj3@gmail.com>
2024-11-22 21:39:26 +0530

    Update direct_download.php without SHD

--- websites/mediafire.com/direct_download.php
+++ websites/mediafire.com/direct_download.php
@@ -1,19 +1,38 @@
 <?php
-include '../shd.php';
-
-
-if(isset($_GET['id'])){
-    $url = 'https://www.mediafire.com/file/'.$_GET['id'].'/';
+if (isset($_GET['id'])) {
+    $url = 'https://www.mediafire.com/file/' . $_GET['id'] . '/';
     $status = 1;
-}else{$url="";$status=0;echo "URL unavailable - try ?url={{your dev.to/sh20raj article url}}";die();}
+} else {
+    $url = "";
+    $status = 0;
+    echo "URL unavailable - try ?url={{your dev.to/sh20raj article url}}";
+    die();
+}
 
+// Fetch the HTML content of the page
+$html = file_get_contents($url);
+if (!$html) {
+    echo "Failed to fetch the URL content.";
+    die();
+}
 
-//$url = "https://dev.to/";
-$status = 1;
+// Load HTML into DOMDocument
+$dom = new DOMDocument();
+libxml_use_internal_errors(true); // Suppress warnings for invalid HTML
+$dom->loadHTML($html);
+libxml_clear_errors();
 
-$html = file_get_html($url);
+// Find the download button by its ID
+$xpath = new DOMXPath($dom);
+$button = $xpath->query("//*[@id='downloadButton']");
 
-$directlink = $html->find('#downloadButton',0)->href;
-
-header('location:'.$directlink.'');
+if ($button->length > 0) {
+    // Extract the href attribute
+    $directlink = $button->item(0)->getAttribute('href');
+    header('Location: ' . $directlink);
+    exit;
+} else {
+    echo "Download button not found!";
+    die();
+}
 ?>
